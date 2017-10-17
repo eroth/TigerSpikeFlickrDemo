1eeb4926cb8b00c4dc1c592647ccb0ec96cea114
Evan Roth <eroth0101@gmail.com>
2017-10-17 08:39:12 +0100

    Split out model objects into separate classes and rename.

--- TigerSpikeFlickrGallery/Model/FlickrPhoto.swift
+++ TigerSpikeFlickrGallery/Model/FlickrPhoto.swift
@@ -1,37 +1,14 @@
 //
-//  FlickrPhotoData.swift
+//  FlickrPhoto.swift
 //  TigerSpikeFlickrGallery
 //
-//  Created by Evan Roth on 10/16/17.
+//  Created by Evan Roth on 10/17/17.
 //  Copyright © 2017 Evan Roth. All rights reserved.
 //
 
 import Foundation
 
-class FlickrRecentPhotos {
-	var lastModifiedDate: String?
-	private var dataArr = [String : Any]()
-	var photoData = [FlickrPhoto]()
-	
-	init(dataArr: [String : Any]) {
-		self.dataArr = dataArr
-		
-		if let lastModifiedDate = dataArr["modified"] {
-			self.lastModifiedDate = String(describing: lastModifiedDate)
-		}
-		
-		var deserializedPhotoData = [FlickrPhoto]()
-		if let photoDataArray = self.dataArr["items"] as? [[String : Any]] {
-			for photoData in photoDataArray {
-				let deserializedPhoto = FlickrPhoto(photoData: photoData)
-				deserializedPhotoData.append(deserializedPhoto)
-			}
-			self.photoData = deserializedPhotoData
-		}
-	}
-}
-
-struct FlickrPhoto {
+class FlickrPhoto {
 	var title: String?
 	var link: String?
 	var mediaURL: String?
